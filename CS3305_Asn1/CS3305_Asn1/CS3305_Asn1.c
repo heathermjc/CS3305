816d713cee5c11f3fb5e00cf146e1beb2ce6e0c2
heathermjc <heathermjc@gmail.com>
2015-02-06 17:14:27 -0500

    second round

--- CS3305_Asn1/CS3305_Asn1/CS3305_Asn1.c
+++ CS3305_Asn1/CS3305_Asn1/CS3305_Asn1.c
@@ -11,6 +11,9 @@
 #define MAX_CMD_ARG 20
 #define MAX_LENGTH 1024
 
+#define PIPES false
+#define REDIRECTION true
+
 // returns false for pipes, true for redirection
 // if there is no operators, then numops will be 0
 bool findtype(char *line, int* numops, int* numcmds)
@@ -38,7 +41,7 @@ void separateCommands(char* line, char** argv)
 		while (*pline == '|' || *pline == '>' || *pline == '<')
 			*pline++ = '\0';     //replace these with 0, so that operands now signify end of a string
 		*argv++ = pline;          // save the argument position
-		printf("s: %s\n", *(argv-1));
+		printf("s: %s\n", *(argv - 1));
 		while (*pline != '\0' && *pline != '|' &&
 			*pline != '>' && *pline != '<')
 			pline++;             //keep skipping through pline to go through all of it
@@ -48,13 +51,13 @@ void separateCommands(char* line, char** argv)
 
 char** tokenizeCmd(char *line)
 {
-	char** argv = malloc( sizeof(char*) * MAX_CMD_ARG );
-char** argv2 = &argv;
+	char** argv = malloc(sizeof(char*) * MAX_CMD_ARG);
+	char** argv2 = &argv;
 	while (*line != '\0') {       //while there is still line 
 		while (*line == ' ' || *line == '\t' || *line == '\n')
 			*line++ = '\0';     //replace these with 0, so that it's saved as the end of the word
 		*argv++ = line;          // save the arg position
-		printf("t: %s\n", *(argv-1));
+		printf("t: %s\n", *(argv - 1));
 		while (*line != '\0' && *line != ' ' &&
 			*line != '\t' && *line != '\n')
 			line++;             //keep skipping through line to go through all of it
@@ -74,7 +77,7 @@ void parse(char *line, char ****argv) //
 	int i = 0;
 	while (linet[i] != NULL) {
 		*(argv[i]) = tokenizeCmd(linet[i]);
-		i++;	
+		i++;
 	}
 
 }
@@ -86,39 +89,101 @@ int main(void)
 	char line[MAX_LENGTH];
 	char *cmd[20];
 
-		while (1) {
-			printf("heather> ");
-			//main loop: read a line. Parse the command, execute the pipeline.
-			// To parse the command, can  write recursive parses or use yavv(1) to generate
-			gets(line);
-			printf("\n");
-			int numops = 0;
-			int numcmds = 0;
-			bool cmdType = findtype(line, &numops, &numcmds); //Find type of commands (False if pipes, True if redirection)
-			printf("%s\n",line);			
-			parse(line, &argv);
-			printf("numops; %d, numcmds; %d, cmdType; %s\n",numops,numcmds,cmdType==true?"true":"false");
+	while (1) {
+		printf("heather> ");
+		//main loop: read a line. Parse the command, execute the pipeline.
+		// To parse the command, can  write recursive parses or use yavv(1) to generate
+		gets(line);
+		printf("\n");
+		int numops = 0;
+		int numcmds = 0;
+		bool cmdType = findtype(line, &numops, &numcmds); //Find type of commands (False if pipes, True if redirection)
+		printf("%s\n", line);			//test
+		parse(line, &argv);
+		printf("numops; %d, numcmds; %d, cmdType; %s\n", numops, numcmds, cmdType == true ? "true" : "false"); //test
 
-			printf("%s\n", argv[0][0]);
-			printf("%s\n", **argv);
-			
-			for (int i = 0; i < 5; i++) {
-				while (*argv[i] != NULL) {
-					printf("%s\n",*argv[i]); 
-					argv[i]++;		
+		printf("%s\n", argv[0][0]);
+		printf("%s\n", **argv);
+
+		for (int i = 0; i < 5; i++) {
+			while (*argv[i] != NULL) {
+				printf("%s\n", *argv[i]);
+				argv[i]++;
+			}
+		}
+
+		if (strstr(line, "exit") != NULL)
+			exit(0);
+		else if (strstr(line, "history") != NULL) {
+			//TODO: history loop *************************************
+			//by default this command will display last 10 commands issued
+			//if there are fewer than 10 commands will display all previously issued
+			//history command may take a numberical argument eg history 5
+			return 0;
+		}
+		else if (cmdType == PIPES) {
+			//pipes
+		}
+		else if (cmdType == REDIRECTION) {
+			//redirection
+		}
+		else {
+			//if it's a regular command, no pipes
+		}
+	}
+	return 0;
+}
+
+int pipes(char **args, int pipes) {
+	int commands = pipes + 1;
+	int i = 0;
+	int pipefds[2 * pipes];
+	for (i = 0; i < pipes; i++){
+		if (pipe(pipefds + i * 2) < 0) {
+			perror("Pipe error");
+			exit(0);
+		}
+	}
+	int pid;
+	int status;
+	int j = 0;
+	for (i = 0; i < (commands - 1); i++) {
+		pid = fork();
+		if (pid == 0) { //if it is not the last command
+			if (i < commands-2) {
+				if (dup2(pipefds[j + 1], 1) < 0) {
+					perror("dup2 issue");
+					exist(0);
 				}
 			}
 
-			if (strstr(line, "exit") != NULL)
-				exit(0);
-			else if (strstr(line, "history") != NULL) {
-				//TODO: history loop *************************************
-				//by default this command will display last 10 commands issued
-				//if there are fewer than 10 commands will display all previously issued
-				//history command may take a numberical argument eg history 5
-				return 0;
+			if (j != 0){ //not first command
+				if (dup2(pipefds[j - 2], 0) < 0) {
+					perror("dup2");
+					exit(0);
+				}
 			}
-			//else pipes(argv);
+
+			for (i = 0; i < 2 * pipes; i++) {
+				close(pipefds[i]);
+			}
+
+			if (execvp(**args) < 0) {
+				perror("error");
+				eixt(0);
+			}
+			//need something here to exit if there are 0 commands?
 		}
-	return 0;
+		else if (pid < 0){
+			perror("pipe error");
+			exit(0);
+		}
+		j += 2;
+	}
+	for (i = 0; i < 2 * pipes; i++) {
+		close(pipefds[i]);
+	}
+	for (i = 0; i < pipes + 1; i++) {
+		wait(&status);
+	}
 }
